c2e25122ab5afe652b8a93cff677647312f67f58
Brandon Lee <brandon.02lee@gmail.com>
2024-07-31 15:37:48 +0800

    Build: Add sass plugin

--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,17 @@ module.exports = {
                     filename: 'static/[name][ext]',
                 }
             },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    "style-loader",
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    // Compiles Sass to CSS
+                    "sass-loader",
+                ],
+            },
         ],
     },
     mode: 'production',
