88a61065ac8243ac5a47a9f432041250237a9cea
Brandon Lee <brandon.02lee@gmail.com>
2024-08-03 15:44:16 +0800

    Fix: Fix sidebar responsiveness
    This change makes the offcanvas behaviour
    exclusive to devices with screens <= 992px. On
    larger screens, clicking off sidebar no longer
    closes it.

--- src/index.js
+++ src/index.js
@@ -20,6 +20,15 @@ document.addEventListener("DOMContentLoaded", function () {
     const sidebar = document.querySelector(".sidebar");
     const body = document.querySelector("body");
 
+    function getDim() {
+        window.vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+        window.vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
+    };
+
+    getDim();
+
+    window.addEventListener("resize", getDim);
+
     toggleButton.addEventListener("click", function (e) {
         body.classList.toggle("body--toggle");
         sidebar.classList.toggle("hide");
@@ -29,7 +38,7 @@ document.addEventListener("DOMContentLoaded", function () {
         sidebar.classList.add("hide");
     });
     body.addEventListener("click", function (e) {
-        if (body.classList.contains("body--toggle") && !sidebar.contains(e.target)) {
+        if (vw <= 992 && body.classList.contains("body--toggle") && !sidebar.contains(e.target)) {
             body.classList.remove("body--toggle");
             sidebar.classList.add("hide");
             e.stopPropagation();
